e2a6427f2b65e774bfd673deef5f7a5913230da1
-Denux <timon.klinkert@gmail.com>
2022-09-21 22:17:37 +0200

    added support for tables when writing tomls

--- src/main/java/dev/denux/dtp/internal/writer/TomlWriter.java
+++ src/main/java/dev/denux/dtp/internal/writer/TomlWriter.java
@@ -4,18 +4,24 @@ import dev.denux.dtp.exception.TomlWriteException;
 import dev.denux.dtp.utils.PrimitiveUtil;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class TomlWriter {
 
-    public final Object object;
+    private final Object object;
+    private final Class<?>[] subClasses;
 
     public TomlWriter(Object object) {
         this.object = object;
+        subClasses = object.getClass().getDeclaredClasses();
     }
 
     public String writeToString() {
         StringBuilder builder = new StringBuilder();
         Field[] fields = object.getClass().getDeclaredFields();
+        Map<Class<?>, Field> subClasses = new HashMap<>();
         for (int i = 0; i < fields.length; i++) {
             Field field = fields[i];
             try {
@@ -37,15 +43,44 @@ public class TomlWriter {
                     handleNumber(field, fieldObj, builder);
                 } else if (Boolean.class.equals(clazz)) {
                     handleOther(field, fieldObj, builder);
+                } else if (typeIsClazz(clazz)) {
+                    subClasses.put(clazz, field);
+                    continue;
                 }
             } catch (IllegalAccessException ignored) {
-                continue;
+                ignored.printStackTrace();
             }
             if (i != fields.length - 1) {
                 builder.append("\n");
             }
         }
+        try {
+            Set<Map.Entry<Class<?>, Field>> entrySet = subClasses.entrySet();
+            if (entrySet.size() != 0) {
+                builder.append("\n");
+            }
+            for (Map.Entry<Class<?>, Field> entry : entrySet) {
+                Class<?> clazz = entry.getKey();
+                Field field = entry.getValue();
+                if (clazz.getDeclaredFields().length != 0) {
+                    builder.append("\n").append("[").append(field.getName()).append("]").append("\n");
+                    builder.append(new TomlWriter(field.get(object)).writeToString());
+                }
+            }
+        } catch (IllegalAccessException ignored) {
+            ignored.printStackTrace();
+        }
         return builder.toString();
+    }
+
+    private boolean typeIsClazz(Class<?> clazz) {
+        for (Class<?> subClass : subClasses) {
+            subClass = PrimitiveUtil.wrap(subClass);
+            if (subClass.equals(clazz)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void handleString(Field field, Object fieldObject, StringBuilder builder) {
--- src/test/java/dev/denux/dtp/WriteObject.java
+++ src/test/java/dev/denux/dtp/WriteObject.java
@@ -2,9 +2,15 @@ package dev.denux.dtp;
 
 public class WriteObject {
 
+    private final WriteClass writeClass = new WriteClass();
+
     private final String testString = "Hallo welt";
     private int integer;
     private final boolean isRight = true;
     private final double notANumber = Double.NaN;
     private final double infinity = Double.POSITIVE_INFINITY;
+
+    private static class WriteClass {
+        private final String subClassTest = "This tests something, can you believe it?";
+    }
 }
